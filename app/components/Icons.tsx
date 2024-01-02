39f647dfe3cc3fa9d5458632b509eab41952ef47
JayDeep-Giri <jaydeepgiri30@gmail.com>
2024-01-02 23:19:45 +0530

    Icons export

---
+++ app/components/Icons.tsx
@@ -0,0 +1,25 @@
+export const SunIcon = () => {
+  return (
+    <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+<g id="SVGRepo_bgCarrier" stroke-width="0"/>
+
+<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+
+<g id="SVGRepo_iconCarrier"> <circle cx="12" cy="12" r="6" stroke="#c5cad8" stroke-width="1.5"/> <path d="M12 2V3" stroke="#c5cad8" stroke-width="1.5" stroke-linecap="round"/> <path d="M12 21V22" stroke="#c5cad8" stroke-width="1.5" stroke-linecap="round"/> <path d="M22 12L21 12" stroke="#c5cad8" stroke-width="1.5" stroke-linecap="round"/> <path d="M3 12L2 12" stroke="#c5cad8" stroke-width="1.5" stroke-linecap="round"/> <path d="M19.0708 4.92969L18.678 5.32252" stroke="#c5cad8" stroke-width="1.5" stroke-linecap="round"/> <path d="M5.32178 18.6777L4.92894 19.0706" stroke="#c5cad8" stroke-width="1.5" stroke-linecap="round"/> <path d="M19.0708 19.0703L18.678 18.6775" stroke="#c5cad8" stroke-width="1.5" stroke-linecap="round"/> <path d="M5.32178 5.32227L4.92894 4.92943" stroke="#c5cad8" stroke-width="1.5" stroke-linecap="round"/> </g>
+
+</svg>
+  );
+};
+
+export const MoonIcon = () => {
+  return (
+    <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.589 21.659c-3.873 1.038-8.517-.545-10.98-3.632a1 1 0 0 1 .751-1.623c3.984-.118 6.662-1.485 8.17-4.098 1.51-2.613 1.354-5.616-.535-9.125a1 1 0 0 1 1.03-1.463c3.904.59 7.597 3.82 8.635 7.694 1.43 5.334-1.737 10.818-7.071 12.247z" fill="#000000" /></svg>
+  );
+};
+
+export const BackArrowIcon = () => {
+  return (
+    <svg fill="#a955f7" width="80px" height="30px" viewBox="0 0 32 32" data-name="Layer 2" id="Layer_2" xmlns="http://www.w3.org/2000/svg"><title/><path d="M11.17,10.23a33.37,33.37,0,0,0-3.05,3.13c-.51.62-1.28,1.3-1.21,2.17s.81,1.24,1.35,1.76a16.3,16.3,0,0,1,2.57,3.17c.86,1.36,3,.11,2.16-1.26a21.06,21.06,0,0,0-1.82-2.48A16.16,16.16,0,0,0,10,15.52c-.22-.21-.86-1.14-.68-.49l-.13,1a17.85,17.85,0,0,1,3.72-4c1.19-1.08-.58-2.85-1.77-1.76Z"/><path d="M9.4,17a109.13,109.13,0,0,0,12.53-.1c1.59-.11,1.61-2.61,0-2.5a109.13,109.13,0,0,1-12.53.1c-1.61-.07-1.6,2.43,0,2.5Z"/></svg>
+  );
+}
